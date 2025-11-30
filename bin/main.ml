54f7310931256c35cf54d38f8e1f602fb6be5a49
Paul Horikx <p.horikx@gmail.com>
2025-11-30 14:11:16 +0100

    added read lines function

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,5 @@
-let () = print_endline "Hello, World!"
+let lines = Adventofcode_2025.Common.read_input "fixtures/day1/input_test.txt";;
+let f elem = 
+  print_endline elem
+in 
+  List.iter f lines;;
