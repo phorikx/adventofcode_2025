0af70a8a695870dba4add85ffabbb21535315caa
Paul Horikx <p.horikx@gmail.com>
2025-11-30 13:16:15 +0100

    initial commit

---
+++ lib/common.ml
@@ -0,0 +1,7 @@
+let read_line i = try Some(input_line i) with End_of_file -> None
+
+let read_input path = 
+  let rec lines_from_files_aux i acc = match (read_line i) with
+    | None -> List.rev acc
+    | Some s -> lines_from_files_aux i (s :: acc) in
+  lines_from_files_aux (open_in path) []
