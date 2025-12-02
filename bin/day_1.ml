b26a8397bbd797c5afbd094f28974b14578af096
Paul Horikx <p.horikx@gmail.com>
2025-12-02 20:34:14 +0100

    completed day 1 b

--- bin/day_1.ml
+++ bin/day_1.ml
@@ -13,10 +13,32 @@ let new_total = if new_point = 0 then total + 1
 else total in
   (new_point, new_total)
 
+let count_passing_zeroes ((point, total) : int * int) (s: string) : int * int =
+  let val_leng = String.length s - 1 in
+  let shift = String.sub s 1 val_leng |>
+    int_of_string in
+  let new_point = if s.[0] = "L".[0] then (point - shift)
+    else if s.[0] = "R".[0] then (point + shift)
+    else raise (CharacterInputError "Correct character not found") in
+  let new_point_mod = Adventofcode_2025.Common.modulo new_point 100 in
+  let point_div = new_point / 100 in
+  let adder = if new_point <= 0 && point <> 0 then - point_div + 1
+    else if new_point < 0 && point = 0 then - point_div
+    else point_div in
+  let new_total = total + adder in
+    (new_point_mod, new_total)
 
-let result = List.fold_left count_zeroes (50, 0) lines
 
-let () = result |>
+let result_a = List.fold_left count_zeroes (50, 0) lines
+
+let result_b = List.fold_left count_passing_zeroes (50, 0) lines
+
+let () = result_a |>
+  snd |>
+  string_of_int |>
+  print_endline
+
+let () = result_b |>
   snd |>
   string_of_int |>
   print_endline
